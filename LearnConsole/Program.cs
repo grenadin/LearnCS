4ae0fccccaec5eac1ac18c782873a00c9ed67d04
grenadin <harirak@hotmail.co.th>
2021-04-17 03:31:24 +0700

    Add project files.

---
+++ LearnConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
